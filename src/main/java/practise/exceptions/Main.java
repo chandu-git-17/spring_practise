eb38cf4ddce385b465196d0feac3f11be3e97905
chandu-git-17 <chandrashekar.yamasani@gmail.com>
2026-02-23 12:47:57 +0530

    Started Exceptions

---
+++ src/main/java/practise/exceptions/Main.java
@@ -0,0 +1,39 @@
+package practise.exceptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ExceptionPractise ep = new ExceptionPractise();
+        ep.number1 = 5;
+        ep.number2 = 1;
+        try{
+            System.out.println(ep.print());
+        }
+        catch(ArithmeticException a){
+            System.out.println("Arithmetic exception occurred");
+        }
+
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(null);
+        ep.list = list;
+
+        try{
+            if(ep.arrayCalculations().isPresent()){
+                System.out.println(ep.arrayCalculations().get());
+            }
+        }
+        catch (ArithmeticException ae){
+            System.out.println("Arithmetic exception occurred");
+        }
+        catch (NullPointerException n){
+            System.out.println("Null Pointer Exception");
+        }
+
+    }
+
+}
