2357857b123736ab5ec78b958816558c3cc34870
chandu-git-17 <chandrashekar.yamasani@gmail.com>
2026-02-26 15:48:13 +0530

    Volatile Keyword

--- src/main/java/practise/passbyvalue/Main.java
+++ src/main/java/practise/passbyvalue/Main.java
@@ -11,6 +11,8 @@ public class Main {
         System.out.println(a);
     }
     public static void changeNumber(PassByValue pbv){
+        pbv = new PassByValue();
+        pbv.number = 12;
     }
 
     static void printSomething(Long id){
